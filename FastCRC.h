c483e01db1628ec0244f679c5e763c242668fb36
Manuel <manuel.cargnel@rwth-aachen.de>
2018-09-21 15:11:02 +0200

    Update FastCRC.h

--- FastCRC.h
+++ FastCRC.h
@@ -88,6 +88,7 @@ public:
   uint8_t smbus(const uint8_t *data, const uint16_t datalen);		// Alias CRC-8
   uint8_t maxim(const uint8_t *data, const uint16_t datalen);		// Equivalent to _crc_ibutton_update() in crc16.h from avr_libc
   
+  uint8_t f12_upd(const uint8_t *data, uint16_t datalen);			// Call for subsequent calculations with previous seed.
   uint8_t smbus_upd(const uint8_t *data, uint16_t datalen);			// Call for subsequent calculations with previous seed.
   uint8_t maxim_upd(const uint8_t *data, uint16_t datalen);			// Call for subsequent calculations with previous seed.
 #if !CRC_SW
